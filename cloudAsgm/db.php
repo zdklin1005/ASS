07e4f0f135ecbc5ec19adc1986955305bac5fadb
Weisheng0925 <yeapws-pm23@student.tarc.edu.my>
2025-05-02 16:07:59 +0800

    Update db.php

--- cloudAsgm/db.php
+++ cloudAsgm/db.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("连接失败: " . $e->getMessage());
+    die("Faill HAHAH: " . $e->getMessage());
 }
 ?>
