ffb863427153ce1c10d6f98913a3264ed05ecd79
Kenny Yeoh Kai Xian <kennyykx-pm23@student.tarc.edu.my>
2025-05-02 12:56:59 +0800

    Update graduation_store.sql

--- graduation_store.sql
+++ graduation_store.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2025 at 03:05 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
