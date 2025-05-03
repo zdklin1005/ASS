6eac66963ad3c2764ccd3a7307f5bc7323be572c
yijia30 <ongyj-pm23@student.tarc.edu.my>
2025-05-04 00:30:08 +0800

    Update index.php

--- cloudAsgm/index.php
+++ cloudAsgm/index.php
@@ -14,7 +14,7 @@ try {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Graduation Store</title>
+    <title>Gownzilla Shop</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <style>
         body {
